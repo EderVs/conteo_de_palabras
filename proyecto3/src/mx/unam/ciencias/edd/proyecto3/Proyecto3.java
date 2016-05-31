d7142d57f4d010942293bc989814f92b861194a1
edervs <eedervs.97@gmail.com>
2016-05-30 19:33:07 -0500

    Cuerpo del proyecto.

---
+++ proyecto3/src/mx/unam/ciencias/edd/proyecto3/Proyecto3.java
@@ -0,0 +1,17 @@
+package mx.unam.ciencias.edd.proyecto3;
+
+import mx.unam.ciencias.edd.*;
+
+/**
+  * <p>Proyecto 3</p>
+  *
+  * @author Eder Vs
+  * @version 1.0
+  * @since 14/06/2016
+  *
+  */
+public class Proyecto3 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
